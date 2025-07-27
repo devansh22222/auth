a89cdcaefe04776ed33e213976ffcf2bea626455
Devansh Oad <devanshoad123@gmail.com>
2025-07-27 12:17:43 +0530

    Added some changes

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const { register, login, logout } = require("../controllers/authController");
-const userAuth = require("../middleware/userAuth");
+
 
 const authRouter = express.Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
 authRouter.post("/logout", logout);
-authRouter.post("/logout", userAuth);
+
 
 
 module.exports = authRouter;
